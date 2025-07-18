129ce75a08f9084bb9eb71ecbb75a4291e1e5068
Maximilien Gilet <maximilien.gilet@externe.maif.fr>
2025-07-18 16:41:15 +0200

    improve visual

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -53,9 +53,9 @@ require("lazy").setup({
 })
 
 vim.cmd("colorscheme cyberdream")
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 
 require("lspconfig").eslint.setup({})
 
