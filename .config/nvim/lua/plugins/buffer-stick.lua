347bdac4117e077e8e2022b57219530d61a7b79a
Maximilien Gilet <maximilien.gilet@externe.maif.fr>
2025-10-01 11:45:18 +0200

    buffer-stick

---
+++ .config/nvim/lua/plugins/buffer-stick.lua
@@ -0,0 +1,25 @@
+return {
+  "ahkohd/buffer-sticks.nvim",
+  event = "VeryLazy",
+  keys = {
+    {
+      "<leader>j",
+      function()
+        BufferSticks.jump()
+      end,
+      desc = "Buffer jump mode",
+    },
+  },
+  config = function()
+    local sticks = require("buffer-sticks")
+    sticks.setup({
+      filter = { buftypes = { "terminal" } },
+      highlights = {
+        active = { link = "Statement" },
+        inactive = { link = "Comment" },
+        label = { link = "Comment" },
+      },
+    })
+    sticks.show()
+  end,
+}
